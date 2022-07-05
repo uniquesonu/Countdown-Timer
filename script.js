ee6b4bcb6c0feb02164a6beff428325fc3271d65
Sonu <100422918+uniquesonu@users.noreply.github.com>
2022-07-05 14:19:56 +0530

    Done

---
+++ script.js
@@ -0,0 +1,29 @@
+let days_element = document.querySelector('#days');
+let hours_element = document.querySelector('#hours');
+let minutes_element = document.querySelector('#minutes');
+// let seconds_element = document.querySelector('#second');
+const seconds_element = document.querySelector('#Second');
+
+const newYear = '1 jan 2023';
+
+function countdown(){
+    const newYearDate = new Date (newYear);
+    const currentDate = new Date();
+
+    const totalSecond = (newYearDate - currentDate)/1000;
+    const days = Math.floor(totalSecond/3600/24);
+    const hours = Math.floor(totalSecond/3600)%24;
+    const minutes = Math.floor(totalSecond/60)%60;
+    const seconds = Math.floor(totalSecond)%60;
+
+    // console.log(days,hours,minutes,seconds);
+    
+    days_element.innerHTML = days;
+    hours_element.innerHTML = hours;
+    minutes_element.innerHTML = minutes;
+    seconds_element.innerHTML = seconds;
+
+}
+
+countdown();
+setInterval(countdown,1000);
